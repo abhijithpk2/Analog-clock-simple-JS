c8e2cb6646900bb56a17f09cb61f29aa4d4e6e11
Abhijith PK <pkabhijith1@gmail.com>
2022-09-10 21:44:45 +0530

    final

---
+++ script.js
@@ -0,0 +1,20 @@
+const hourHand=document.getElementById('hr')
+const minuteHand=document.getElementById('mn')
+const secondHand=document.getElementById('se')
+
+setInterval(setClock,1000)
+
+function setClock(){
+    const currentDate= new Date()
+    const secondsRatio= currentDate.getSeconds()/60
+    const minutesRatio= (secondsRatio + currentDate.getMinutes())/60
+    const hoursRatio= (minutesRatio + currentDate.getHours())/12
+
+    setRotation(secondHand,secondsRatio)
+    setRotation(minuteHand,minutesRatio)
+    setRotation(hourHand,hoursRatio)
+}
+
+function setRotation(element,rotationRatio){
+    element.style.setProperty('--rotation',rotationRatio * 360)
+}
